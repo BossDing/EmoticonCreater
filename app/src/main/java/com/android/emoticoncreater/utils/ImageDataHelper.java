39e9c39e2b477a4d4c7f524f1162f21ed630b8ff
chenxuebin <799536767@qq.com>
2018-09-12 15:54:38 +0800

    #增加图片

--- app/src/main/java/com/android/emoticoncreater/utils/ImageDataHelper.java
+++ app/src/main/java/com/android/emoticoncreater/utils/ImageDataHelper.java
@@ -76,7 +76,8 @@ public class ImageDataHelper {
             R.raw.img_xiongmaoren49, R.raw.img_xiongmaoren50, R.raw.img_xiongmaoren51,
             R.raw.img_xiongmaoren52, R.raw.img_xiongmaoren53, R.raw.img_xiongmaoren54,
             R.raw.img_xiongmaoren55, R.raw.img_xiongmaoren56, R.raw.img_xiongmaoren57,
-            R.raw.img_xiongmaoren58, R.raw.img_xiongmaoren59, R.raw.img_xiongmaoren60
+            R.raw.img_xiongmaoren58, R.raw.img_xiongmaoren59, R.raw.img_xiongmaoren60,
+            R.raw.img_xiongmaoren61, R.raw.img_xiongmaoren62, R.raw.img_xiongmaoren63
     };
     public static final int[] HUA_JI_LIST = {
             R.raw.img_huaji1, R.raw.img_huaji2, R.raw.img_huaji3,
